99310b6ae1fcd357d7007a3bb4ffc0685c50d23d
Valentyn Pogrebinsky <pva@isd.com.ua>
2016-12-26 23:46:55 +0200

    destructuring fixed

--- test/es6-parameter-destructuring.js
+++ test/es6-parameter-destructuring.js
@@ -1,4 +1,23 @@
-var assert = require('chai').assert;
+var expect = require('chai').expect;
 
 describe('es6 parameter destructuring', function() {
+  it("With ES6 & a function parameter's default value", () => {
+
+    function drawES6Chart({size : size, cords : cords, radius: radius}
+        = {size : 'big', cords : {x : 0, y : 0}, radius : 25 }
+    /* // TODO: Provide the parameter desctructuring to satisfy all assertions below  */) {
+      console.log(size, cords, radius);
+      return [size, cords, radius];
+    }
+    let actual = drawES6Chart();
+    expect(actual).deep.equal(['big', { x: 0, y: 0 }, 25]);
+
+    actual = drawES6Chart({
+      size: 'big',
+      cords: { x: 18, y: 30 },
+      radius: 30
+    });
+    expect(actual).deep.equal(['big', { x: 18, y: 30 }, 30]);
+
+  });
 });
